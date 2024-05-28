41c75d2ef281e9a9bec3dc103d01d75ae9d85610
salman k <salman371k@gmail.com>
2024-05-28 17:59:56 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['@headlessui/react'],
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'redux-vendor': ['react-redux', '@reduxjs/toolkit'],
-          // Removed @headlessui/react from manualChunks
-          // You can further split other chunks if needed
-        },
-        chunkFileNames: 'static/js/[name].[hash].js',
-        entryFileNames: 'static/js/[name].[hash].js',
-        assetFileNames: 'static/assets/[name].[hash].[ext]',
-      },
     },
-    chunkSizeWarningLimit: 1000, // Increase the limit if necessary
   },
 });
 
